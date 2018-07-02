da67bbf6832e160359924e3e52a9de4fdc711797
Sung J Byun <sungjbyun@D-10-19-184-2.dhcp4.washington.edu>
2018-07-01 21:14:33 -0700

    set up all the basic setting with navBar

---
+++ client/src/components/AppNavBar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+  } from 'reactstrap';
+
+export default class AppNavBar extends Component {
+  constructor(props) {
+    super(props);
+    // instead binding functions, you can adopt ES6 way in function
+    // this.toggle = this.toggle.bind(this);
+    // this state actually can be extracted and stand alone not enclosed in constructor
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className='mb-5'>
+          <Container>
+            <NavbarBrand href="/">Shopping List for Debbie</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className='ml-auto' navbar>
+                <NavItem>
+                  <NavLink href='https://github.com/sjb3'target="_blank" rel="noopener noreferrer">
+                    GitHub
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    )
+  }
+}
