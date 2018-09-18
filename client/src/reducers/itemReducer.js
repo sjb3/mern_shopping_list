e483ad18d2c54b0da79ac1402c0cdc8fb869ff27
Sung J Byun <sungjbyun@D-10-19-63-16.dhcp4.washington.edu>
2018-09-17 19:30:08 -0700

    here to fix mlab issue

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -2,24 +2,27 @@
 // items from the ShoppingList are moved here
 // eventually these coming from the back-end
 
-import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from '../actions/types';
+import {
+  GET_ITEMS, ADD_ITEMS, DELETE_ITEMS, ITEMS_LOADING,
+} from '../actions/types';
 
 const initialState = {
-  items: [
-    {
-      id: uuid(), desc: 'No more craps', name: 'Dior Suit', URI: 'http://ftape.com/media/wp-content/uploads/2013/09/Dior-Homme-FW13-Suit.jpg',
-    },
-    {
-      id: uuid(), desc: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', name: 'Givenchy Jacket', URI: 'https://monetanddolce.files.wordpress.com/2012/12/givenchy-haute-couture-2010-fall-winter.jpg',
-    },
-    {
-      id: uuid(), desc: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', name: 'Chanel Suit', URI: 'https://totallist.files.wordpress.com/2013/01/chanel-suit.jpg',
-    },
-    {
-      id: uuid(), desc: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', name: 'Margiela Coat', URI: 'https://a.1stdibscdn.com/archivesE/upload/v_1402/21_15/img_9187/IMG_9187_l.jpeg',
-    },
-  ],
+  items: [],
+  loading: false,
+  // items: [
+  //   {
+  //     id: uuid(), desc: 'No more craps', name: 'Dior Suit', URI: 'http://ftape.com/media/wp-content/uploads/2013/09/Dior-Homme-FW13-Suit.jpg',
+  //   },
+  //   {
+  //     id: uuid(), desc: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', name: 'Givenchy Jacket', URI: 'https://monetanddolce.files.wordpress.com/2012/12/givenchy-haute-couture-2010-fall-winter.jpg',
+  //   },
+  //   {
+  //     id: uuid(), desc: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', name: 'Chanel Suit', URI: 'https://totallist.files.wordpress.com/2013/01/chanel-suit.jpg',
+  //   },
+  //   {
+  //     id: uuid(), desc: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', name: 'Margiela Coat', URI: 'https://a.1stdibscdn.com/archivesE/upload/v_1402/21_15/img_9187/IMG_9187_l.jpeg',
+  //   },
+  // ],
 };
 
 export default function (state = initialState, action) {
@@ -27,6 +30,8 @@ export default function (state = initialState, action) {
     case GET_ITEMS:
       return {
         ...state,
+        items: action.payload,
+        loading: false,
       };
     case DELETE_ITEMS:
       return {
@@ -37,6 +42,11 @@ export default function (state = initialState, action) {
       return {
         ...state,
         items: [action.payload, ...state.items],
+      };
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true,
       };
     default:
       return state;
